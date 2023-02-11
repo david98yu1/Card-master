1d180896ae2d261bbe2dd886eaec3ee3a8f5550c
Mei Xiaohan <sc17xhm@leeds.ac.uk>
2023-02-10 18:59:41 -0800

    second

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public static PlayerController instance;
+
+    [SerializeField]
+    private float speed = 1.0f;
+
+    private Rigidbody2D rb;
+    private float input;
+    private BoxCollider2D boxCollider;
+
+    public float acc;
+    
+    //fall and restart
+    private Vector3 respawnPoint; //recall where palyer restart
+    public GameObject fallDetector; //link the script to FallDetector
+    
+    
+    
+
+    private void Awake()
+    {
+        if (PlayerController.instance == null) { PlayerController.instance = this; }
+        else { Destroy(this); }
+    }
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        boxCollider = GetComponent<BoxCollider2D>();
+        
+        respawnPoint = transform.position;
+    }
+
+    private void Update()
+    {
+        rb.velocity = new Vector2(acc * speed, rb.velocity.y);
+
+        fallDetector.transform.position = new Vector2(transform.position.x, fallDetector.transform.position.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "FallDetector")
+        {
+            transform.position = respawnPoint;
+        }
+        else if (collision.tag == "Checkpoint")
+        {
+            respawnPoint = transform.position;
+        }
+    }
+    
+    public void Jump()
+    {
+        rb.velocity = new Vector2(rb.velocity.x, 10 * speed);
+    }
+}
